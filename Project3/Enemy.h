ffb1ebb5cdd780bc5e24a3127ebeb3e47d67c06c
Jinouga47 <45074493+Jinouga47@users.noreply.github.com>
2022-03-07 16:14:38 +0000

    Basic Movement

--- Project3/Enemy.h
+++ Project3/Enemy.h
@@ -1,1 +1,16 @@
 #pragma once
+
+class Enemy
+{
+	public:
+		Enemy();
+		Enemy(int stage, int health);
+
+		~Enemy();
+		void DealDamage();
+		virtual void Movement() = 0;
+
+	private:
+		int hp;
+		//RenderItem struct //For keeping track of Geometry Object
+};
